d8ce7717f8c96c9994c842ea801227f65b2ec3b4
zsy-uestc <1162411462@qq.com>
2024-06-04 16:33:36 +0800

    新增数据库,保存flamegraph运行的相关信息

---
+++ build/CMakeFiles/myflamegraph.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/myflamegraph.dir/src/showFlameGraph.cpp.o"
+  "libmyflamegraph.a"
+  "libmyflamegraph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/myflamegraph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
